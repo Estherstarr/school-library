1ff2d4b51c7cdd49478068a5230226bbfe94ad5a
onomeh <onwuanibeonome@gmail.com>
2022-07-29 15:16:55 +0000

    Create id , change method name

--- person.rb
+++ person.rb
@@ -4,6 +4,7 @@ class Person
   attr_accessor :name, :age
 
   def initialize(age:, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
@@ -15,7 +16,7 @@ class Person
 
   private
 
-  def of_age?
-    age >= 18
+  def is_of_age?
+    @age >= 18
   end
 end
