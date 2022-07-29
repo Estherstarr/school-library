e85ae2a974ec99a4e126655f94552e936502dc67
onomeh <onwuanibeonome@gmail.com>
2022-07-29 15:55:15 +0000

    Remove redundant space

--- student.rb
+++ student.rb
@@ -1,6 +1,6 @@
 # Defines a student class
 class Student < Person
-  def initialize(age:, classroom:,  name: 'Unknown', parent_permission: true)
+  def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(age: age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
