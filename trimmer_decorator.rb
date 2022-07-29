a198a965e36906c9ac97ac7e720371dea50c9b54
onomeh <onwuanibeonome@gmail.com>
2022-07-29 17:26:33 +0000

    Create trimmer decorator class

---
+++ trimmer_decorator.rb
@@ -0,0 +1,12 @@
+# Defines a trimmer_decorator class
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    name = @nameable.correct_name
+
+    if name.length > 10
+      name[0, 10]
+    else
+      name
+    end
+  end
+end
