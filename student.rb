dd3f6b0d4e10f6a9d7bf04b05bc77b2560305748
onomeh <onwuanibeonome@gmail.com>
2022-07-29 13:56:41 +0000

    Create Student class

---
+++ student.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Defines a student class
+class Student < Person
+  def initialize(age:, classroom:)
+    super(age: age)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
