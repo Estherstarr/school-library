6e3c552588a7089ecab8e20c2d9b42ff7a441ba0
onomeh <onwuanibeonome@gmail.com>
2022-07-30 06:59:35 +0000

    Add associations to rental class

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require_relative 'nameable'
 
 # Defines a person class
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def initialize(age:, name: 'Unknown', parent_permission: true)
@@ -11,6 +11,7 @@ class Person < Nameable
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def correct_name
@@ -19,6 +20,11 @@ class Person < Nameable
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def add_rental(rental)
+    @rentals << rental
+    rental.person = self
   end
 
   private
