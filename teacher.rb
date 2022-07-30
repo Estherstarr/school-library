066ff7a73a6f03b69a6e99dd503bf359c68d2608
onomeh <onwuanibeonome@gmail.com>
2022-07-30 13:33:41 +0000

    Fix linter error

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 # Defines a teacher class
 class Teacher < Person
   attr_reader :specialization
-  
+
   def initialize(age:, specialization:, name: 'Unknown', parent_permission: true)
     super(age: age, name: name, parent_permission: parent_permission)
     @specialization = specialization
