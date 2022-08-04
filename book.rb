411baaeb94c02b0dbc6f1d343c79eba99a9df928
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-08-04 14:12:37 +0100

    Fix linters error

--- book.rb
+++ book.rb
@@ -1,16 +1,21 @@
+# frozen_string_literal: true
+
 # Defines a Book class
 class Book
   attr_accessor :title, :author
   attr_reader :rentals
+
   def initialize(title:, author:)
     @title = title
     @author = author
     @rentals = []
   end
+
   def add_rental(rental)
     @rentals << rental
     rental.book = self
   end
+
   def to_h
     {
       title: title,
